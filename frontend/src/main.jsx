d5023d299634b7bd31a2873cb6c1677e41ab376c
Zherikhov <39011402+Zherikhov@users.noreply.github.com>
2025-08-23 12:53:28 +0200

    fix: resolve merge conflict in main entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import App from './App.jsx'
 import BoardsPage from './pages/BoardsPage.jsx'
 import BoardPage from './pages/BoardPage.jsx'
 import './index.css'
@@ -9,10 +10,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<App/>}/>
         <Route path="/boards" element={<BoardsPage/>}/>
         <Route path="/boards/:roomId" element={<BoardPage/>}/>
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
