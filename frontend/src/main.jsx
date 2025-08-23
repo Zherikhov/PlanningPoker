9a2496cdfa9dd4acaa6a48797807e735f49650b1
Zherikhov <39011402+Zherikhov@users.noreply.github.com>
2025-08-23 13:02:41 +0200

    refactor: redirect root to boards page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import App from './App.jsx'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import BoardsPage from './pages/BoardsPage.jsx'
 import BoardPage from './pages/BoardPage.jsx'
 import './index.css'
@@ -10,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}/>
+        <Route path="/" element={<Navigate to="/boards" replace />} />
         <Route path="/boards" element={<BoardsPage/>}/>
         <Route path="/boards/:roomId" element={<BoardPage/>}/>
         <Route path="*" element={<div>404</div>} />
